4e2f8a5201a4e625fa08902800c3cad9e97ef7fc
sansi <sansiex1988@gmail.com>
2014-06-04 21:19:15 +0800

    exec

---
+++ src/main/java/com/dianping/sansi/interpreter/controller/ScriptExecutor.java
@@ -0,0 +1,17 @@
+package com.dianping.sansi.interpreter.controller;
+
+import org.javacc.parser.JavaCCParser;
+
+/**
+ * Created by sansi on 2014/6/4.
+ */
+public class ScriptExecutor {
+
+    public void exec(String script){
+        //JavaCCParser p=new JavaCCParser();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/dianping/sansi/interpreter/interpreter/Interpreter.java
@@ -0,0 +1,8 @@
+package com.dianping.sansi.interpreter.interpreter;
+
+/**
+ * Created by sansi on 2014/6/4.
+ */
+public interface Interpreter {
+    void interpret(String script);
+}
